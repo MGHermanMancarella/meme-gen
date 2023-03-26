992d68232ec91f9383beedc555526ac1a130e130
Michael <mherman315@gmail.com>
2023-03-25 22:12:17 -0700

    Getting closer.. Trying to figure out how to use an icon for the delete button

--- script.js
+++ script.js
@@ -2,35 +2,48 @@ const myForm = document.querySelector('form');
 
 let memeDepot = document.getElementById('memeDepot');
 
-function checkIfNull(val) {
-  if (val === null) {
-    return undefined;
-  }
-}
 myForm?.addEventListener('submit', function (event) {
   event.preventDefault();
 
-  const imgURL = document.getElementById('imageURL');
   // const color = document.getElementById('textColor');
   const topText = document.getElementById('topText');
   const bottomText = document.getElementById('bottomText');
+  const imgURL = document.getElementById('imageURL');
+  if (imgURL.value === '') {
+    return alert(
+      "Even Bob Ross couldn't paint without a canvas. Give me a URL"
+    );
+  }
 
   const memeDiv = document.createElement('div');
   const topDiv = document.createElement('div');
   const bottomDiv = document.createElement('div');
   const newImg = document.createElement('img');
+  const deleteButton = document.createElement('button');
 
-  memeDiv.setAttribute('class', 'meme container');
+  memeDiv.setAttribute('class', 'meme');
   topDiv.innerText = topText.value;
   topDiv.setAttribute('class', 'top');
   bottomDiv.innerText = bottomText.value;
   bottomDiv.setAttribute('class', 'bottom');
   newImg.src = imgURL.value;
-  console.log(memeDiv.getAttribute('class'));
+  // console.log(memeDiv.getAttribute('class'));
+  deleteButton.setAttribute('class', 'button');
+  const deleteIcon = document.createElement('img');
+  deleteIcon.src = 'remove.svg';
+  deleteButton.appendChild(deleteIcon);
 
   memeDiv.appendChild(newImg);
   memeDiv.appendChild(topDiv);
   memeDiv.appendChild(bottomDiv);
+  memeDiv.appendChild(deleteButton);
   memeDepot.appendChild(memeDiv);
   myForm.reset();
+  const memes = document.querySelectorAll('button.button');
 });
+for (let meme of memes) {
+  meme.addEventListener('click', function (event) {
+    let GetOutaHere = event.target;
+    GetOutaHere.parentNode.parentNode.removeChild(GetOutaHere.parentNode);
+  });
+}
